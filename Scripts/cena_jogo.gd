b4b5db1217abd7ac44a76ca3cd48b61144096ca2
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2024-11-15 13:14:14 -0300

    mapa feito

--- Scripts/cena_jogo.gd
+++ Scripts/cena_jogo.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
 var telaCheia: bool = false
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func abilitarFullscreen():
+	if telaCheia:
+		telaCheia = false
+	else:
+		telaCheia = true
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_restart"):
 		get_tree().change_scene_to_file("res://Scenes/cena_jogo.tscn")
@@ -9,11 +15,12 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
 	
-	if Input.is_action_just_pressed("fullscreen"):
+	if Input.is_action_just_pressed("ui_fullscreen"):
 		if !telaCheia:
 			DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN)
-			telaCheia = true
-		if telaCheia:
+			#await get_tree().create_timer(1).timeout
+			abilitarFullscreen()
+		else:
 			DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_WINDOWED)
-			telaCheia = false
-		
+			#await get_tree().create_timer(1).timeout
+			abilitarFullscreen()
