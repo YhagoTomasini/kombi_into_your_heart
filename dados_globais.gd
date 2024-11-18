60170f5cc790e9ae86fd83be8b3e16d73a6a53a7
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2024-11-17 22:23:46 -0300

    so coisa boa... soq n

--- dados_globais.gd
+++ dados_globais.gd
@@ -2,3 +2,5 @@ extends Node
 
 var score = 0
 var highScores = 0
+
+#var arrosarChao: bool = false
--- hearts.gd
+++ hearts.gd
@@ -5,4 +5,5 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Kombi":
 		print("Vida extra")
 		DadosGlobais.score += 1
+		#DadosGlobais.arrosarChao = true
 		queue_free()
